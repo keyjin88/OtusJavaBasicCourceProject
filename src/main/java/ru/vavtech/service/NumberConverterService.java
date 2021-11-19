deec449c274457c43c3854ea1e666cb09033ac0c
AV <keyjin88@gmail.com>
2021-11-19 14:23:46 +0300

    Вынес беквенные представления чисел в константы

--- src/main/java/ru/vavtech/service/NumberConverterService.java
+++ src/main/java/ru/vavtech/service/NumberConverterService.java
@@ -2,10 +2,11 @@ package ru.vavtech.service;
 
 import ru.vavtech.model.InputNumber;
 import ru.vavtech.model.enums.Currency;
-import ru.vavtech.model.enums.Number;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static ru.vavtech.model.enums.NumberConstants.*;
 
 /**
  * Класс обрабатывает полученное число и приводит его к строке
@@ -48,7 +49,7 @@ public class NumberConverterService {
         var stringRepresentationOfNumber = new StringBuilder();
 
         if (inputNumber.getIntegerPart() == 0) {
-            stringRepresentationOfNumber.append(Number.ZERO.getSingleValue());
+            stringRepresentationOfNumber.append(ZERO);
         } else {
             stringRepresentationOfNumber.append(convertNumberToString(inputNumber.getIntegerPart(), Currency.CURRENCY));
         }
@@ -89,7 +90,7 @@ public class NumberConverterService {
 
             if (rankCounter > 0)
                 sb.insert(0,
-                        result + Number.RANKS_ENDINGS.getTwoDimensionalValuesArray()[Math.min(rankCounter, 2)][numbersRanksEndingsIndex]);
+                        result + RANKS_ENDINGS.get(Math.min(rankCounter, 2)).get(numbersRanksEndingsIndex));
             else
                 sb.insert(0, result);
 
@@ -108,7 +109,7 @@ public class NumberConverterService {
      */
     private String convertPartNumberToString(int number, Currency currency) {
         var stringBuffer = new StringBuilder();
-        stringBuffer.append(Number.RANKS.getValuesArray().get(rankCounter));
+        stringBuffer.append(RANKS.get(rankCounter));
         var listOfNumbers = convertNumberToList(number);
         var extraNumber = number % 100;
         if (extraNumber > 10 && extraNumber < 20) {
@@ -126,10 +127,10 @@ public class NumberConverterService {
      */
     private void convertPartNumberToStringWithExtraNumber(List<Integer> listOfNumbers, StringBuilder stringBuffer) {
         var index = listOfNumbers.get(0);
-        stringBuffer.insert(0, Number.PRIME_FROM_11_TO_19.getValuesArray().get(index));
+        stringBuffer.insert(0, PRIME_FROM_11_TO_19.get(index));
         if (listOfNumbers.size() > 2) {
             var hundred = listOfNumbers.get(2);
-            stringBuffer.insert(0, Number.PRIME_NUMBERS_DOZENS_AND_HUNDREDS.getTwoDimensionalValuesArray()[2][hundred]);
+            stringBuffer.insert(0, PRIME_NUMBERS_DOZENS_AND_HUNDREDS.get(2).get(hundred));
         }
         numbersRanksEndingsIndex = 2;
     }
@@ -146,26 +147,26 @@ public class NumberConverterService {
             if (i == 0) {
                 switch (index) {
                     case 1 -> {
-                        stringBuffer.insert(0, Number.PRIME_NUMBERS_DOZENS_AND_HUNDREDS.getTwoDimensionalValuesArray()[i][index]);
-                        stringBuffer.insert(2, rankCounter == 1 ? Number.RANKS_ENDINGS.getTwoDimensionalValuesArray()[i][0] : currency.getPrimeNumbersEndings().get(0));
+                        stringBuffer.insert(0, PRIME_NUMBERS_DOZENS_AND_HUNDREDS.get(i).get(index));
+                        stringBuffer.insert(2, rankCounter == 1 ? RANKS_ENDINGS.get(i).get(0) : currency.getPrimeNumbersEndings().get(0));
                         numbersRanksEndingsIndex = 0;
                     }
                     case 2 -> {
-                        stringBuffer.insert(0, Number.PRIME_NUMBERS_DOZENS_AND_HUNDREDS.getTwoDimensionalValuesArray()[i][index]);
-                        stringBuffer.insert(2, rankCounter == 1 ? Number.RANKS_ENDINGS.getTwoDimensionalValuesArray()[i][1] : currency.getPrimeNumbersEndings().get(1));
+                        stringBuffer.insert(0, PRIME_NUMBERS_DOZENS_AND_HUNDREDS.get(i).get(index));
+                        stringBuffer.insert(2, rankCounter == 1 ? RANKS_ENDINGS.get(i).get(1) : currency.getPrimeNumbersEndings().get(1));
                         numbersRanksEndingsIndex = 1;
                     }
                     case 3, 4 -> {
-                        stringBuffer.insert(0, Number.PRIME_NUMBERS_DOZENS_AND_HUNDREDS.getTwoDimensionalValuesArray()[i][index]);
+                        stringBuffer.insert(0, PRIME_NUMBERS_DOZENS_AND_HUNDREDS.get(i).get(index));
                         numbersRanksEndingsIndex = 1;
                     }
                     default -> {
-                        stringBuffer.insert(0, Number.PRIME_NUMBERS_DOZENS_AND_HUNDREDS.getTwoDimensionalValuesArray()[i][index]);
+                        stringBuffer.insert(0, PRIME_NUMBERS_DOZENS_AND_HUNDREDS.get(i).get(index));
                         numbersRanksEndingsIndex = 2;
                     }
                 }
             } else {
-                stringBuffer.insert(0, Number.PRIME_NUMBERS_DOZENS_AND_HUNDREDS.getTwoDimensionalValuesArray()[i][index]);
+                stringBuffer.insert(0, PRIME_NUMBERS_DOZENS_AND_HUNDREDS.get(i).get(index));
             }
         }
     }
